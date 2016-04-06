9fb599f516a2b2a096eb03c238da2af49f52ba41
shjie <295973776@qq.com>
2016-04-06 20:06:51 +0800

    B commit changed

---
+++ GitDemo/GitDemo/ViewControllerB.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerB.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/4/6.
+//  Copyright © 2016年 Jeson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerB : UIViewController
+
+@end
