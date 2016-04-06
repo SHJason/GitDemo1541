4a50fb9bbf15968a105581f0e973531944bebe59
shjie <295973776@qq.com>
2016-04-06 17:57:59 +0800

    first commit pro

---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/4/6.
+//  Copyright © 2016年 Jeson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by qianfeng on 16/4/6.
+//  Copyright © 2016年 Jeson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
